562704b0b81d9fd9e9c3c5a422e5be77dadcd44b
Victor <victorluz.lima@gmail.com>
2022-01-25 22:41:16 -0300

    initial commit

---
+++ src/controllers/searchController.ts
@@ -0,0 +1,17 @@
+import { createMenuObject } from './../helpers/createMenuObjects';
+import {Request, Response} from 'express';
+import { Pet } from '../models/pet';
+
+export const search = (req: Request, res: Response)=>{
+    let query: string = req.query.q as string;
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+    let list = Pet.getFromName(query);
+    res.render('pages/page',{
+        menu: createMenuObject(''),
+        list,
+        query
+    });
+}
